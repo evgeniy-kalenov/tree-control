ad01a1231707fbb21a84e3d8272c5763f0db35f2
Evgeniy <evg.kalenov@yandex.ru>
2020-12-14 22:14:00 +0500

    feat: add views to tree

--- src/components/Tree/Tree.tsx
+++ src/components/Tree/Tree.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { ITreeNode, TreeActions } from "./types";
 import { defaultTree } from "../../data";
-import TreeNode from "./TreeNode";
+import TreeNode, { Link } from "./TreeNode";
 import genKey from "../../utils/genKey";
 import { IAction } from "../../types";
+import styled from "styled-components";
 
 export default function Tree(): JSX.Element {
 
@@ -27,9 +28,56 @@ export default function Tree(): JSX.Element {
         }
     };
 
+    const jsonNode = useRef<HTMLPreElement>();
+    const [ isTree, setIsTree ] = useState<boolean>(true);
+
+    const treeView = (ev) => {
+        ev.preventDefault();
+        setIsTree(true);
+    };
+
+    const jsonView = (ev) => {
+        ev.preventDefault();
+        setIsTree(false);
+    };
+
+    useEffect(() => {
+        if (!isTree) {
+            const element = document.createTextNode(JSON.stringify(tree, null, 4));
+            jsonNode.current.appendChild(element);
+        }
+    }, [isTree]);
+
+    /*
+    * Renders
+    * */
+
     return (
-        <div>
-            <TreeNode value={ tree } parent={ null } onChange={ changeTree } />
-        </div>
+        <Container>
+            <LinksContainer>
+                <ViewLink href={"#"} onClick={ treeView } selected={ isTree }>TREE</ViewLink>
+                <ViewLink href={"#"} onClick={ jsonView } selected={ !isTree }>JSON</ViewLink>
+            </LinksContainer>
+            {
+                isTree
+                    ? <TreeNode value={ tree } parent={ null } onChange={ changeTree } />
+                    : <pre ref={ jsonNode }></pre>
+            }
+        </Container>
     );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const LinksContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const ViewLink = styled(Link)<{ selected: boolean }>`
+  text-decoration: ${ ({ selected }) => !selected && "none" };
+`;
